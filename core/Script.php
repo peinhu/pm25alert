1558cd5c552ffdbc9d33ffd5b6cfe8b78660b52b
peinhu <hu_yang110@163.com>
2016-12-29 21:47:33 +0800

    update composer script

--- core/Script.php
+++ core/Script.php
@@ -8,13 +8,11 @@ class Script
 {
 
     public static function testTime(Event $event)
-    {
-        $installedPackage = $event->getOperation()->getPackage();
-        
+    {   
 		date_default_timezone_set('Asia/Shanghai');
 
 		if(exec('date "+%H:%M"  ')===date('H:i',time())){
-			echo 'OK'.PHP_EOL;
+			echo 'OK:时区检测通过。'.PHP_EOL;
 		}else{
 			echo 'Error:请检查系统时区设置，PHP和系统时间不一致会导致订阅者在奇怪的时间收到告警。'.PHP_EOL;
 		}
