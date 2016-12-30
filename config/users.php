71dc405d531e438cb760dab8aad428050afd9f21
Payne <hu_yang110@163.com>
2016-12-30 14:55:24 +0800

    Update users.php

--- config/users.php
+++ config/users.php
@@ -3,7 +3,7 @@
 # 订阅此服务的用户，可添加多个
 
 return [
-
+    //请求http://www.pm25.in/api/querys/station_names.json?token=5j1znBVAsnSf5xQyNQyq&city={CITY_NAME}，{CITY_NAME}为城市名，得到城市中所有监测点及其编号{STATION_CODE}
     [
         'name' => '',//'John Doe'
 
@@ -13,7 +13,7 @@ return [
 
         'alert_level'=>1,//1~6或42(即大于6)
 
-        'api_url'=>''//'http://www.pm25.in/api/querys/aqis_by_station.json?station_code=1166A&token=5j1znBVAsnSf5xQyNQyq'
+        'api_url'=>''//'http://www.pm25.in/api/querys/aqis_by_station.json?token=5j1znBVAsnSf5xQyNQyq&station_code={STATION_CODE}'
     ],
 
 
